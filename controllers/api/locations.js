26794f3c648cf68362ace9a503688b4d36b03beb
Christian Battaglia <christian.d.battaglia@gmail.com>
2017-03-01 11:26:30 -0500

    database up and running

---
+++ controllers/api/locations.js
@@ -0,0 +1,10 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const db = new sqlite3.Database(path.join(__dirname, '../../genie.sqlite'));
+
+exports.all = function(req, res) {
+	db.all("SELECT * FROM tblLR", function(err, rows) {
+		res.json(rows);
+	});
+};
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,18 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const db = new sqlite3.Database(path.join(__dirname, '../genie.sqlite'));
+
 /**
  * GET /
  * Home page.
  */
 
 exports.index = function(req, res) {
+
+	db.all("SELECT * FROM tblIR", function(err, rows) {
+		// TO DO SOMETHING HERE
+	});
+
 	res.render('layout', {});
 };
